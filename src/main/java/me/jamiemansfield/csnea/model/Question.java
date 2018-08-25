cf868b3f9aca7b99ecdcdd5051a4636773187cad
Jamie Mansfield <dev@jamierocks.uk>
2018-08-25 14:41:56 +0100

    Release under the Mozilla Public License 2.0

--- src/main/java/me/jamiemansfield/csnea/model/Question.java
+++ src/main/java/me/jamiemansfield/csnea/model/Question.java
@@ -1,3 +1,10 @@
+//******************************************************************************
+// Copyright (c) Jamie Mansfield <https://jamiemansfield.me/>
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//******************************************************************************
+
 package me.jamiemansfield.csnea.model;
 
 import java.util.ArrayList;
